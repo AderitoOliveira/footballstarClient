9deb41a721b85c3c65f94c842717fb4f9f727514
AderitoOliveira <aderito.nelson1@gmail.com>
2020-04-07 15:11:35 +0100

    Added the messageOfTheRoute to the admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  messageOfTheRoute : string;
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
