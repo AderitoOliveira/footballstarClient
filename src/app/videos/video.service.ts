14590cedc4b7e498d4b0887956710623c005cf51
AderitoOliveira <aderito.nelson1@gmail.com>
2020-04-20 08:38:28 +0100

    Added the environment variable ${environment.apiUrl} to be built in the Production phase

--- src/app/videos/video.service.ts
+++ src/app/videos/video.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '@environments/environment';
 
 @Injectable()
 export class VideoService {
@@ -14,15 +15,15 @@ export class VideoService {
       
         console.log("Inside getViedosOfPlayer");
 
-        return this.httpClient.get('http://localhost:3000/allVideosOfPlayer/' + player_id).pipe(map((res:any) => res)) 
+        return this.httpClient.get(`${environment.apiUrl}/allVideosOfPlayer/` + player_id).pipe(map((res:any) => res)) 
     }
 
     uploadVideoInfoToDatabase(player_id : number, file_name : string) {
         //this.httpClient.post('http://localhost:3000/insertVideoInfoToDatabase', { player_id, filename }).pipe(map((res:any) => res)) 
 
-        this.httpClient.post('http://localhost:3000/insertVideoInfoToDatabase', { player_id, file_name }).subscribe({
+        this.httpClient.post(`${environment.apiUrl}/insertVideoInfoToDatabase`, { player_id, file_name }).subscribe({
         //next: data => this.postId = data.id,
         error: error => console.error('There was an error!', error)
-})
+        })
     }
 }
